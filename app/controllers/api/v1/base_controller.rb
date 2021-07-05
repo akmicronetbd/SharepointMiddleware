a83e107e5bb2f4529ab4a214a371b26b92acf64b
AshwinKumar <ashwin@rranktec.com>
2021-07-05 20:33:36 +0530

    bug fix

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::BaseController < ApplicationController
 
   # called before every action on controllers
   before_action :authorize_request
-  
+  skip_before_action :verify_authenticity_token
+
   private
   
   # Check for valid request token and return user
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      post 'share_docuement' => 'file_receives#receive_file'
+      post 'share_document' => 'file_receives#receive_file'
     end
   end
 end
