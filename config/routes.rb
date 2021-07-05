674e1d1608d5035e089fd1f10a23cde296f50987
AshwinKumar <ashwin@rranktec.com>
2021-07-05 20:29:48 +0530

    bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      post 'share_docuement' => 'file_receives#receive_file'
+    end
+  end
 end
