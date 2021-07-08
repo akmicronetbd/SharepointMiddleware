cad2492000452b6f88697aadcddd5c5f52517b10
AshwinKumar <ashwin@rranktec.com>
2021-07-09 00:58:48 +0530

    bug fix

--- app/controllers/api/v1/file_receives_controller.rb
+++ app/controllers/api/v1/file_receives_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::FileReceivesController < Api::V1::BaseController
 	def receive_file
-		@file_receive = FileReceive.create(file_name: params[:file_name], folder_name: params[:folder_name], account_id: params[:account_id])
+		@file_receive = FileReceive.create(account_id: params[:account_id], document_id: params[:document_id], data: request.raw_post)
 		render json: {message: "Success"}, status: 200
 	end
 end
